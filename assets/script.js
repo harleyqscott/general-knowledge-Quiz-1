dbb07f1c8010734f16e58a0679b3df945558f1c5
Harley Scott <harley.scott@students.codeinstitute.net>
2024-09-04 14:01:04 +0000

    changes to css, plus added section on creating a username

--- assets/script.js
+++ assets/script.js
@@ -32,3 +32,16 @@ if(document.getElementById('correct6').checked){
 }
  alert("Your Score is... "+ score);
 }
+
+
+
+//creating username 
+        const txt1 = document.getElementById('username');
+        const btn1 =  document.getElementById('btn1');
+        const out1 = document.getElementById('output1');
+
+        function fun1(){
+            out1.innerHTML = txt1.value;
+        }
+
+        btn1.addEventListener('click',fun1);
